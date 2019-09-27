9fd70683548add554f14d0ccc0f841bc21b426cc
Simon Bramble <simonbramble.digitaldwp@gmail.com>
2019-09-27 16:03:59 +0100

    First content changes based on design review 27/9/19

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Apply to use the Child Maintenance Service',
+  serviceName: 'Apply for help arranging child maintenance',
 
   // Default port that prototype runs on
   port: '3000',
