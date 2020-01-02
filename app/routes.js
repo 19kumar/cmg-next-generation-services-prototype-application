81db3e6c385ad17be6906a1a030ef74ebae7b538
ben raine <ben.raine.digitaldwp@gmail.com>
2020-01-02 16:00:22 +0000

    shared care

--- app/routes.js
+++ app/routes.js
@@ -56,6 +56,8 @@ router.use('/cmg-app/sprint_15/sprint_15_main_prototype', require('./views/cmg-a
 
 router.use('/cmg-app/sprint_16/sprint_16_main_prototype', require('./views/cmg-app/sprint_16/sprint_16_main_prototype/routes'));
 
+router.use('/cmg-app/sprint_17/sprint_17_main_prototype', require('./views/cmg-app/sprint_17/sprint_17_main_prototype/routes'));
+
 router.use('/cmg-app/dev_build/dev_build_main_prototype', require('./views/cmg-app/dev_build/dev_build_main_prototype/routes'));
 
 router.use('/cmg-app/pin_generator/pin_generator_main_prototype', require('./views/cmg-app/pin_generator/pin_generator_main_prototype/routes'));
